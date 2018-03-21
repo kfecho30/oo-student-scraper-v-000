a43f650c212476dccd2387d885fe6721f89725aa
Kelsey Fecho <kfecho30@gmail.com>
2018-03-21 15:45:04 +0000

    automatically backed up by learn

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -34,7 +34,7 @@ class CommandLineInteface
       puts "  linkedin:".colorize(:light_blue) + " #{student.linkedin}"
       puts "  github:".colorize(:light_blue) + " #{student.github}"
       puts "  blog:".colorize(:light_blue) + " #{student.blog}"
-      puts "----------------------".colorize(:magenta)
+      puts "----------------------".colorize(:light-magenta)
     end
   end
 end
