fd57e75e97676a777a499921b208db0a3682c374
Kelsey Fecho <kfecho30@gmail.com>
2018-03-21 15:04:44 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,7 @@ class Scraper
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
     student = {
-      :profile_quote => doc.css(".vitals-container .vitals-text-container .profile-quote"),
+      :profile_quote => doc.css(".vitals-container .vitals-text-container .profile-quote").text,
       :bio => doc.css(".details-container .description-holder p").text
     }
   end
