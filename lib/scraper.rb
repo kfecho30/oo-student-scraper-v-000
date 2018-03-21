4f8705017b76e00a7b2c24d542937b1ec3813e21
Kelsey Fecho <kfecho30@gmail.com>
2018-03-21 14:55:54 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,28 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    students = doc.css(".student-card")
+    students.collect do |student|
+      {:name => student.css(".card-text-container h4").text,
+      :location => student.css(".card-text-container p").text,
+      :profile_url => student.css("a").map{|link| link['href']}[0]}
+    end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    student = {
+      :name => doc.css(".vitals-text-container .profile-name"),
+      :location => doc.css(".vitals-text-container .profile-location"),
+      :profile_url => profile_url
+    }
   end
 
 end
-
