f937758c9d953632299dbd1392362fbe784d617f
Kelsey Fecho <kfecho30@gmail.com>
2018-03-21 15:24:28 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -35,7 +35,7 @@ class Scraper
       :twitter => twitter,
       :linkedin => linkedin,
       :github => github,
-      :blog => blog if !"",
+      :blog => blog unless nil,
       :profile_quote => doc.css(".vitals-container .vitals-text-container .profile-quote").text,
       :bio => doc.css(".details-container .description-holder p").text
     }
