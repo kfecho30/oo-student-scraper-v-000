f02b3753a1c96ea0b960ae807b76e471890dbe30
Kelsey Fecho <kfecho30@gmail.com>
2018-03-21 15:14:43 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,7 +18,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
-    socials = doc.css(".vitals-container .social-icon-container a").map{|link| link['href']}[0]
+    socials = doc.css(".vitals-container .social-icon-container a").map{|link| link['href']}
     student = {
       :twitter => socials,
       :profile_quote => doc.css(".vitals-container .vitals-text-container .profile-quote").text,
